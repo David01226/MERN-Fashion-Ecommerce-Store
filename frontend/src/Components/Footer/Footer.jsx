47d7c2995895907a3dbb8627e75a4647c794a3a4
David01226 <d.gibson1029384756@gmail.com>
2024-10-16 08:15:38 +0100

    Add new collections and newsletter

---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './NewsLetter.css'
+import footer_logo from '../Assets/logo_big.png'
+import instagram_icon from '../Assets/instagram_icon.png'
+import pinterest_icon from '../Assets/pinterest_icon.png'
+import whatsapp_icon from '../Assets/whatsapp_icon.png'
+
+ 
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-logo">
+        <img src={footer_logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="footer-link">
+        <li>Company</li>
+        <li>Products</li>
+        <li>Offices</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <div className="footer-social-icons">
+        <div className="footer-icons-container">
+          <img src={instagram_icon} alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
